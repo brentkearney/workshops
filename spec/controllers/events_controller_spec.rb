a139a755c76b51428d43499f440868a8ebe46bf3
Brent Kearney <brentk@birs.ca>
2016-01-26 18:06:43 -0700

    no database INSERTS!

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -16,16 +16,39 @@ RSpec.describe EventsController, type: :controller do
     end
 
     context 'with authentication' do
-      it "allows authenticated access" do
-        user = FactoryGirl.build(:user)
+      let(:person) { FactoryGirl.build(:person) }
+      let(:user) { FactoryGirl.build(:user, person: person) }
+
+      before do
         allow(request.env['warden']).to receive(:authenticate!).and_return(user)
         allow(controller).to receive(:current_user).and_return(user)
+      end
 
+      it 'responds with success code' do
         get :index
 
         expect(response).to be_success
       end
-    end
 
+      it 'assigns @heading to "Your Events"' do
+        get :index
+
+        expect(assigns(:heading)).to eq('Your Events')
+      end
+
+      it "assigns the authenticated user's events to @events" do
+        event = FactoryGirl.build(:event)
+        # person defined above, associated to authenticated user
+        membership = FactoryGirl.build(:membership, person: person, event: event)
+
+        get :index
+
+        expect(assigns(:events)).to eq(person.events)
+      end
+    end
+  end
+
+  describe '#all' do
+    
   end
 end
