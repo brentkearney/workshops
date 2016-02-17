8950f240ca3e0c19199f64b91127e2d2564d593a
Brent Kearney <brentk@birs.ca>
2016-02-17 15:26:05 -0700

    Copyright change.

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -1,6 +1,7 @@
-# Copyright (c) 2016 Brent Kearney. This file is part of Workshops.
-# Workshops is licensed under the GNU Affero General Public License
-# as published by the Free Software Foundation, version 3 of the License.
+# Copyright (c) 2016 Banff International Research Station.
+# This file is part of Workshops. Workshops is licensed under
+# the GNU Affero General Public License as published by the
+# Free Software Foundation, version 3 of the License.
 # See the COPYRIGHT file for details and exceptions.
 
 class LecturesController < ApplicationController
--- app/services/legacy_connector.rb
+++ app/services/legacy_connector.rb
@@ -1,6 +1,6 @@
-# Copyright (c) 2016 Brent Kearney
+# Copyright (c) 2016 Banff International Research Station
 #
-# This file is part of Workshops.
+# This file is part of Workshops. Workshops is licensed under
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this
 # software and associated documentation files (the "Software"), to deal in the Software
