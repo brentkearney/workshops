a5b023a2b8706cda98d31848ae8f488154958133
Brent Kearney <brentk@birs.ca>
2016-05-19 17:48:56 -0600

    Started refactoring ScheduleItem test.

--- spec/services/schedule_item_spec.rb
+++ spec/services/schedule_item_spec.rb
@@ -7,19 +7,24 @@
 require 'rails_helper'
 
 describe "ScheduleItem" do
-
   before do
     @user = authenticate_user
-    @event = FactoryGirl.create(:event)
-    @schedule_attributes = { :event_id => @event.id,
-                             :start_time => (@event.start_date + 1.days).to_time.in_time_zone(@event.time_zone).change({ hour: 9, min: 0}),
-                             :end_time =>  (@event.start_date + 1.days).to_time.in_time_zone(@event.time_zone).change({ hour: 10, min: 0}),
-                             :name =>  'Good morning!',
-                             :description =>  'The first item of the day.',
-                             :location => 'TCPL 201',
-                             :updated_by => "RSpec" }
-
-    @new_params = {new_item: true, event_id: @event.id, start_time: @event.start_date.to_time, updated_by: @user.name}
+    @event = create(:event)
+    @schedule_attributes = {
+          event_id: @event.id,
+        start_time: monday_at_nine,
+          end_time: (monday_at_nine + 1.hour),
+              name: 'Good morning!',
+       description: 'The first item of the day.',
+          location: 'TCPL 201',
+        updated_by: "RSpec"
+    }
+    @new_params = {
+          new_item: true,
+          event_id: @event.id,
+        start_time: @event.start_date.to_time,
+        updated_by: @user.name
+    }
   end
 
   after(:each) do
@@ -31,42 +36,34 @@ describe "ScheduleItem" do
     Schedule.delete_all
   end
 
-  it "accepts a hash of schedule attributes" do
-    CSI = ScheduleItem.new(@schedule_attributes)
-    expect(CSI.class).to eq(ScheduleItem)
+  def monday_at_nine
+    (@event.start_date + 1.days).to_time.in_time_zone(@event.time_zone).change({ hour: 9, min: 0})
   end
 
-  describe "Updates existing Schedule Items" do
-    it 'accepts a schedule object and new params' do
-      schedule = ScheduleItem.new(@schedule_attributes.merge(name: 'Original title')).schedule
-      # original_schedule = Schedule.create!(schedule.attributes)
-      # expect(original_schedule.name).to eq('Original title')
-
-      params = @schedule_attributes.merge(name: 'New title')
-      new_schedule = ScheduleItem.update(schedule, params)
-      expect(new_schedule['name']).to eq('New title')
-    end
+  it '.new' do
+    csi = ScheduleItem.new(@schedule_attributes)
+    expect(csi.class).to eq(ScheduleItem)
   end
 
-  describe "Creates new Schedule Items" do
-    it ".set_default_location sets location based on event_type" do
-      @event.event_type = 'Research in Teams'
+  it '.update' do
+    schedule = ScheduleItem.new(@schedule_attributes.merge(name: 'Original title')).schedule
+
+    params = @schedule_attributes.merge(name: 'New title')
+    new_schedule = ScheduleItem.update(schedule, params)
+
+    expect(new_schedule['name']).to eq('New title')
+  end
+
+  describe '.new(params).schedule' do
+    it '.set_default_location sets the room according to the event type & location' do
+      new_event_type = Global.event.types.third
+      @event.event_type = new_event_type
       @event.save
 
       item = ScheduleItem.new(@new_params.merge(event_id: @event.id)).schedule
-      expect(item.location).to eq('TCPL 107')
 
-      @event.event_type = 'Focussed Research Group'
-      @event.save
-      new_params = {new_item: true, event_id: @event.id, start_time: @event.start_date.to_time, updated_by: @user.name}
-      schedule = ScheduleItem.new(new_params).schedule
-      expect(schedule.location).to eq('TCPL 202')
-
-      @event.event_type = '5 Day Workshop'
-      @event.save
-      new_params = {new_item: true, event_id: @event.id, start_time: @event.start_date.to_time, updated_by: @user.name}
-      schedule = ScheduleItem.new(new_params).schedule
-      expect(schedule.location).to eq('TCPL 201')
+      new_room = Global.location.rooms.send(@event.location).send(new_event_type)
+      expect(item.location).to eq(new_room)
     end
 
     context "With no nested lecture_attributes" do
@@ -99,7 +96,7 @@ describe "ScheduleItem" do
 
       context "with a person_id" do
         it "should create a valid schedule and associated lecture object" do
-          person = FactoryGirl.create(:person)
+          person = create(:person)
           @schedule_attributes[:lecture_attributes] = { :person_id => person.id }
           schedule = ScheduleItem.new(@schedule_attributes).schedule
 
@@ -124,19 +121,19 @@ describe "ScheduleItem" do
       context "there are previously scheduled lectures" do
         before do
           @event.schedules.delete_all
-          @day = @event.start_date + 1.days
-          lecture_attributes = FactoryGirl.build(:lecture, event: @event).attributes
+          @day = monday_at_nine
+          lecture_attributes = build(:lecture, event: @event).attributes
           (9..12).each do |t|
-            lecture_time = @day.to_time.in_time_zone(@event.time_zone).change({ hour: t, min: 0})
+            lecture_time = @day.change({ hour: t, min: 0})
             lecture = Lecture.new(lecture_attributes.merge(start_time: lecture_time, end_time: lecture_time + 30.minutes))
-            FactoryGirl.create(:schedule, event: @event, lecture: lecture, start_time: lecture.start_time, end_time: lecture.end_time)
+            create(:schedule, event: @event, lecture: lecture, start_time: lecture.start_time, end_time: lecture.end_time)
           end
         end
 
         it "should use the same length of a lecture scheduled at the same time the previous day" do
           previous_start = (@day + 1.days).to_time.in_time_zone(@event.time_zone).change({ hour: 9, min: 0 })
           previous_end = (@day + 1.days).to_time.in_time_zone(@event.time_zone).change({ hour: 9, min: 23 })
-          previous_item = FactoryGirl.create(:lecture, event_id: @event.id, start_time: previous_start, end_time: previous_end)
+          previous_item = create(:lecture, event_id: @event.id, start_time: previous_start, end_time: previous_end)
 
           new_item = ScheduleItem.new(@new_params.merge(event_id: @event.id, start_time: (@day + 2.days).to_time.in_time_zone(@event.time_zone).change({ hour: 9, min: 0 }))).schedule
           s = Schedule.create(new_item.attributes)
@@ -159,10 +156,14 @@ describe "ScheduleItem" do
           end
         end
 
+        def event_timezone_at(hour)
+          @event.start_date.to_time.in_time_zone(@event.time_zone).change({ hour: hour, min: 0})
+        end
+
         context "and there are no scheduled non-lectures either" do
           it "should set the start_time to 09:00" do
             new_item = ScheduleItem.new(@new_params.merge(event_id: @event.id)).schedule
-            expect(new_item.start_time).to eq(@event.start_date.to_time.in_time_zone(@event.time_zone).change({ hour: 9, min: 0}))
+            expect(new_item.start_time).to eq(event_timezone_at(9))
           end
         end
 
@@ -189,7 +190,7 @@ describe "ScheduleItem" do
             schedule = Schedule.create!(@schedule_attributes.
                           merge(start_time: "#{@event.start_date} 17:00", end_time: "#{@event.start_date} 17:30"))
             new_item = ScheduleItem.new(@new_params.merge(event_id: @event.id)).schedule
-            expect(new_item.start_time).to eq(@event.start_date.to_time.in_time_zone(@event.time_zone).change({ hour: 9, min: 0}))
+            expect(new_item.start_time).to eq(event_timezone_at(9))
           end
 
           it "unless there are preceding items, then it should use the first available slot" do
@@ -213,15 +214,15 @@ describe "ScheduleItem" do
           it "should set the start_time to the most popular start time" do
             day = @event.start_date + 1.day
             lecture_time = day.to_time.in_time_zone(@event.time_zone).change({ hour: 14, min: 0})
-            lecture1 = FactoryGirl.create(:lecture, event: @event, start_time: lecture_time, end_time: lecture_time + 30.minutes)
-            FactoryGirl.create(:schedule, event: @event, lecture: lecture1, start_time: lecture1.start_time, end_time: lecture1.end_time)
+            lecture1 = create(:lecture, event: @event, start_time: lecture_time, end_time: lecture_time + 30.minutes)
+            create(:schedule, event: @event, lecture: lecture1, start_time: lecture1.start_time, end_time: lecture1.end_time)
 
-            lecture2 = FactoryGirl.create(:lecture, event: @event, start_time: lecture_time + 1.day, end_time: lecture_time + 1.day + 30.minutes)
-            FactoryGirl.create(:schedule, event: @event, lecture: lecture2, start_time: lecture2.start_time, end_time: lecture2.end_time)
+            lecture2 = create(:lecture, event: @event, start_time: lecture_time + 1.day, end_time: lecture_time + 1.day + 30.minutes)
+            create(:schedule, event: @event, lecture: lecture2, start_time: lecture2.start_time, end_time: lecture2.end_time)
 
             other_lecture_time = (lecture_time + 2.days) - 3.hours
-            lecture3 = FactoryGirl.create(:lecture, event: @event, start_time: other_lecture_time, end_time: other_lecture_time + 30.minutes)
-            FactoryGirl.create(:schedule, event: @event, lecture: lecture3, start_time: lecture3.start_time, end_time: lecture3.end_time)
+            lecture3 = create(:lecture, event: @event, start_time: other_lecture_time, end_time: other_lecture_time + 30.minutes)
+            create(:schedule, event: @event, lecture: lecture3, start_time: lecture3.start_time, end_time: lecture3.end_time)
 
             new_lecture_day = day + 3.days
             expect(Schedule.select {|s| s.start_time.to_date == new_lecture_day.to_date }).to be_empty
@@ -237,28 +238,25 @@ describe "ScheduleItem" do
 
       context "There are scheduled Lectures on the given day," do
         before :each do
-          @day = @event.start_date + 2.days
+          @day = monday_at_nine + 1.day
           @event.schedule_on(@day).each do |item|
             item.delete
           end
-          @lecture_attributes = FactoryGirl.build(:lecture, event: @event).attributes
-          @lecture_time = @day.to_time.in_time_zone(@event.time_zone).change({ hour: 14, min: 0})
+          @lecture_attributes = build(:lecture, event: @event).attributes
+          @lecture_time = @day.change({ hour: 14, min: 0})
 
           @lecture1 = Lecture.new(@lecture_attributes.merge(start_time: @lecture_time, end_time: @lecture_time.change({ hour: 14, min: 30})))
-          FactoryGirl.create(:schedule, event: @event, lecture: @lecture1, start_time: @lecture1.start_time, end_time: @lecture1.end_time)
+          create(:schedule, event: @event, lecture: @lecture1, start_time: @lecture1.start_time, end_time: @lecture1.end_time)
         end
 
         it "it should set the start_time to the end_time of the last scheduled lecture" do
-          #@lecture3 = Lecture.new(@lecture_attributes.merge(start_time: @lecture_time.change({ hour: 14, min: 0}), end_time: @lecture_time.change({ hour: 14, min: 30})))
-          #FactoryGirl.create(:schedule, event: @event, lecture: @lecture3, start_time: @lecture3.start_time, end_time: @lecture3.end_time)
-
           new_item = ScheduleItem.new(@new_params.merge(event_id: @event.id, start_time: @day)).schedule
           expect(new_item.start_time).to eq(@lecture1.end_time)
         end
 
         context "and there is a non-lecture scheduled immediately after the lecture" do
           before do
-            @item1 = FactoryGirl.create(:schedule, event: @event, start_time: @lecture1.end_time, end_time: @lecture1.end_time + 30.minutes)
+            @item1 = create(:schedule, event: @event, start_time: @lecture1.end_time, end_time: @lecture1.end_time + 30.minutes)
           end
 
           it "it should set the start_time to the end_time of the non-lecture item" do
@@ -267,11 +265,11 @@ describe "ScheduleItem" do
           end
 
           it "even if there is more than one non-lecture scheduled back-to-back after the lecture" do
-            item2 = FactoryGirl.create(:schedule, event: @event, start_time: @item1.end_time, end_time: @item1.end_time + 30.minutes)
+            item2 = create(:schedule, event: @event, start_time: @item1.end_time, end_time: @item1.end_time + 30.minutes)
             new_item = ScheduleItem.new(@new_params.merge(event_id: @event.id, start_time: @day)).schedule
             expect(new_item.start_time).to eq(item2.end_time)
 
-            item3 = FactoryGirl.create(:schedule, event: @event, start_time: item2.end_time, end_time: item2.end_time + 45.minutes)
+            item3 = create(:schedule, event: @event, start_time: item2.end_time, end_time: item2.end_time + 45.minutes)
             new_item = ScheduleItem.new(@new_params.merge(event_id: @event.id, start_time: @day)).schedule
             expect(new_item.start_time).to eq(item3.end_time)
           end
@@ -279,7 +277,7 @@ describe "ScheduleItem" do
 
         context "and there is a non-lecture scheduled after the preceding lecture, but not immediately after" do
           before do
-            @item1 = FactoryGirl.create(:schedule, event: @event, start_time: @lecture1.end_time + 60.minutes, end_time: @lecture1.end_time + 90.minutes)
+            @item1 = create(:schedule, event: @event, start_time: @lecture1.end_time + 60.minutes, end_time: @lecture1.end_time + 90.minutes)
           end
 
           it "it should set the start_time to the end_time of the previous lecture (not after the non-lecture)" do
@@ -288,8 +286,6 @@ describe "ScheduleItem" do
           end
         end
       end
-
     end
-
   end
 end
