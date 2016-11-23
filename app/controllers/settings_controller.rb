fb365d00c4fafb2835d3e4a9b7daf1012b827898
Brent Kearney <brentk@birs.ca>
2016-11-22 19:09:00 -0700

    Add new fields feature on Settings edit form, Add Setting creates empty hash.

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -24,21 +24,22 @@ class SettingsController < ApplicationController
 
   def update
     authorize @setting
-    updated_setting = update_params["#{@setting.var}"]
-    if @setting.value != updated_setting
-        @setting.value = updated_setting
-        @setting.save
-        redirect_to settings_path, notice: 'Setting has been updated.'
-      else
-        redirect_to settings_path
-      end
+
+    @setting.value = update_values
+    if @setting.save
+      redirect_to edit_setting_path(params[:id]),
+        notice: 'Setting has been updated.'
+    else
+      redirect_to settings_path,
+        error: 'There was a problem saving the setting.'
+    end
   end
 
   def create
-    setting = Setting.new(setting_params)
-    authorize setting
-    if setting.save
-      redirect_to settings_path, notice: %(Added "#{setting.var}" setting!)
+    @setting = Setting.new(setting_params)
+    authorize @setting
+    if @setting.save
+      redirect_to settings_path, notice: %(Added "#{@setting.var}" setting!)
     else
       flash[:error] = %(Error saving setting: #{setting.errors})
       render :new
@@ -46,7 +47,8 @@ class SettingsController < ApplicationController
   end
 
   def get_setting
-    @setting = Setting.find_by(var: params[:id]) || Setting.new(var: params[:id])
+    @setting = Setting.find_by(var: params[:id]) ||
+      Setting.new(var: params[:id])
   end
 
   def get_settings
@@ -55,22 +57,34 @@ class SettingsController < ApplicationController
 
   private
 
+  def update_values
+    updated_setting = update_params
+    new_field = updated_setting.delete('new_field')
+    new_value = updated_setting.delete('new_value')
+    unless new_field.empty?
+      updated_setting.merge!("#{new_field}": "#{new_value}")
+    end
+    updated_setting
+  end
+
   def update_params
-    setting_fields = []
+    setting_fields = ['new_field', 'new_value']
     @setting.value.each do |field_name, value|
       setting_fields << field_name
     end
-    params.require(:setting).permit("#{@setting.var}": setting_fields)
+    data = params.require(:setting).permit("#{@setting.var}": setting_fields)
+    data["#{@setting.var}"]
   end
 
   def setting_params
     data = params.require(:setting).permit(:var, :value)
     data['var'] = data['var'].to_s.strip
-    if data['value'] =~ /^\[(.+)\]$/
-      data['value'] = data['value'].gsub(/^\[|\]$/, '').split(',')
-    elsif data['value'].respond_to?(:to_unsafe_h)
-      data['value'] = data['value'].to_unsafe_h
-    end
+    data['value'] = {}
+    # if data['value'] =~ /^\[(.+)\]$/
+    #   data['value'] = data['value'].gsub(/^\[|\]$/, '').split(',')
+    # elsif data['value'].respond_to?(:to_unsafe_h)
+    #   data['value'] = data['value'].to_unsafe_h
+    # end
     data
   end
 end
--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,5 +1,6 @@
 # RailsSettings Model
 class Setting < RailsSettings::Base
+  attr_accessor :new_field, :new_value
   namespace Rails.env
 
 end
