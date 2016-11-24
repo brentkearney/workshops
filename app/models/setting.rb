77330b665b4191a3612c3914b0cd35b788926b6a
Brent Kearney <brentk@birs.ca>
2016-11-23 17:54:33 -0700

    Added support for array values

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,6 +1,15 @@
 # RailsSettings Model
 class Setting < RailsSettings::Base
   attr_accessor :new_field, :new_value
+  before_save :convert_type
   namespace Rails.env
 
+  def convert_type
+    self.value.each do |field_name, field_value|
+      if field_value =~ /^\[(.+)\]$/
+        field_value = field_value.gsub(/^\[|"|\]$/, '').split(',').map(&:strip)
+        self.value = self.value.merge("#{field_name}": field_value)
+      end
+    end
+  end
 end
