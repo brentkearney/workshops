17a28c56bef3a5d21bdc2405124eae35499ba138
Brent Kearney <brentk@birs.ca>
2017-01-26 16:11:23 -0700

    Updated settings_spec

--- spec/features/settings_spec.rb
+++ spec/features/settings_spec.rb
@@ -12,6 +12,13 @@ describe 'Settings page', type: :feature do
     expect(Setting.get_all).not_to be_empty
   end
 
+  def find_sub_tabs(section) 
+    Setting.find_by_var(section).value.keys.each do |tab|
+      tab_pane = 'div.tab-pane#' + tab.to_s
+      expect(page.body).to have_css(tab_pane)
+    end
+  end
+
   context 'As an external user' do
     it 'disallows users who are not logged in' do
       visit settings_path
@@ -22,11 +29,24 @@ describe 'Settings page', type: :feature do
   end
 
   context 'As a member user' do
-    it 'denies access' do
-      person = create(:person)
-      user = create(:user, person: person, role: 'member')
-      login_as user, scope: :user
+    before do
+      @person = create(:person)
+      @user = create(:user, person: @person, role: 'staff')
+      login_as @user, scope: :user
+    end
 
+    after do
+      logout(@user)
+      @person.destroy
+    end
+
+    
+    it 'does not show a link to Settings in the drop-down user menu' do
+      visit root_path
+      expect(page).not_to have_css('ul.dropdown-user li a', text: 'Settings')
+    end
+
+    it 'denies access' do
       visit settings_path
 
       expect(page.body).to have_text('Access denied.')
@@ -35,7 +55,28 @@ describe 'Settings page', type: :feature do
   end
 
   context 'As a staff user' do
-    it 'allows access to the Rooms section'
+    before do
+      @person = create(:person)
+      @user = create(:user, person: @person, role: 'staff')
+      login_as @user, scope: :user
+    end
+
+    after do
+      logout(@user)
+      @person.destroy
+    end
+
+    it 'does not show a link to Settings in the drop-down user menu' do
+      visit root_path
+      expect(page).not_to have_css('ul.dropdown-user li a', text: 'Settings')
+    end
+
+    it 'denies access' do
+      visit settings_path
+
+      expect(page.body).to have_text('Access denied.')
+      expect(current_path).to eq(my_events_path)
+    end
   end
 
   context 'As an admin user' do
@@ -43,6 +84,11 @@ describe 'Settings page', type: :feature do
       @person = create(:person)
       @user = create(:user, person: @person, role: 'admin')
       login_as @user, scope: :user
+    end
+
+    after do
+      logout(@user)
+      @person.destroy
     end
 
     it 'has tabs for each Setting section' do
@@ -93,27 +139,45 @@ describe 'Settings page', type: :feature do
         setting = Setting.find_by_var('Site')
         expect(setting.value['event_types'].class).to eq(Array)
       end
-
-      it 'has a way to delete fields'
     end
 
     context 'Emails tab' do
-      it 'has a sub-tab for each location'
+      before :each do
+        visit edit_setting_path('Emails')
+      end
+
+      it 'has a sub-tab for each location' do
+        find_sub_tabs('Emails')
+      end
     end
 
     context 'Locations tab' do
-      it 'has a sub-tab for each location' do
+      before :each do
         visit edit_setting_path('Locations')
+      end
 
-        Setting.Locations.keys.each do |tab|
-          expect(page).to have_link(tab)
+      it 'has a sub-tab for each location' do
+        find_sub_tabs('Locations')
+      end
+
+      it 'has a form field for each field of the location' do 
+        Setting.Locations.each do |location, properties|
+          properties.each do |field, value|
+            expect(page).to have_field("setting[Locations][#{location}][#{field}]")
+          end
         end
       end
 
-      it 'has a form field for each key:value pair of the location'
-      it 'updates the data in the given field'
-      it 'has an "Add New Field" form that adds new fields'
+      it 'updates the data in the given field' do
+        first_key = Setting.Locations.keys.first
+        first_field = Setting.Locations[first_key].first.first
 
+        expect(Setting.Locations[first_key][first_field]).not_to eq('A new name')
+        fill_in "setting[Locations][#{first_key}][#{first_field}]", with: 'A new name'
+        click_button "Update #{first_key} Settings"
+
+        expect(Setting.find_by_var('Locations').value[first_key][first_field]).to eq('A new name')
+      end
 
       it '"Location code" field updates the key representing that location' do
         visit edit_setting_path('Locations')
@@ -151,17 +215,29 @@ describe 'Settings page', type: :feature do
     end
 
     context 'Rooms tab' do
-      it 'has sub-tabs for each location'
-      it 'has a form for adding new rooms'
+      before :each do
+        visit edit_setting_path('Rooms')
+      end
+
+      it 'has sub-tabs for each location' do
+        find_sub_tabs('Rooms')
+      end
+
     end
 
     context '+/- Setting tab' do
-      it 'has a form for adding new setting sections'
-      it 'a new tab appears for the new setting section'
-      it 'asks if the new section is location-dependent'
-      it 'if location dependent, populates new section with location tabs'
-      it 'the Add New field sub-tab is pre-selected in the new section'
-      it 'has a form for deleting existing setting sections'
+      before :each do
+        visit edit_setting_path('new')
+      end
+
+      it 'has a form for adding new setting sections' do
+        expect(page).to have_css('form#new_setting')
+        expect(page).to have_field('setting[new][new_location]')
+      end
+
+      it 'has a form for deleting existing setting sections' do
+        expect(page).to have_field('setting[new][remove_location]')
+      end
     end
   end
 
