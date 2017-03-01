f5d626a61dac4e7ed7b6ccdfa666eea897644647
Brent Kearney <brent@netmojo.ca>
2017-03-01 16:09:56 -0700

    Renamed settings.sql so postgres image doesn't load it before running init-user-db.sh

--- db/pg-init/init-user-db.sh
+++ db/pg-init/init-user-db.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
+echo
 echo "Setting up $DB_USER and Workshops databases..."
-
+echo
 psql -U "$POSTGRES_USER" -c "CREATE USER $DB_USER WITH ENCRYPTED PASSWORD '$DB_PASS';"
+echo
 
 for db in workshops_test workshops_development workshops_production
 do
@@ -13,7 +15,9 @@ do
 
   psql -U "$POSTGRES_USER" -c "GRANT ALL PRIVILEGES ON DATABASE $db to $DB_USER"
 
-  psql -U "$DB_USER" $db < /docker-entrypoint-initdb.d/settings.sql
+  psql -U "$DB_USER" $db < /docker-entrypoint-initdb.d/settings-sql
 done
 
+echo
 echo "Finished database setup."
+echo
