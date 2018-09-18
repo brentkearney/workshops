81cda8aa36f2ca6ff50147523d97431947d6b84b
Brent Kearney <brent@netmojo.ca>
2018-09-17 18:15:51 -0600

    New Sparkpost authentication based on X-header in webhook

--- app/controllers/griddler/authentication_controller.rb
+++ app/controllers/griddler/authentication_controller.rb
@@ -1,11 +1,11 @@
 class Griddler::AuthenticationController < Griddler::EmailsController
   skip_before_filter :verify_authenticity_token, raise: false
+  before_action :authenticate
   protect_from_forgery with: :null_session
   respond_to :json
-  http_basic_authenticate_with name: 'sparkpost', password: GetSetting.site_setting('SPARKPOST_AUTH_TOKEN')
 
   def incoming
-    if params['_json'][0]['msys'].empty? || params['_json'][0]['msys']['message_event'] != nil
+    if params['_json'][0]['msys'] != nil
       msg = "Received POST on Workshops /maillist interface:\n\n #{params.inspect}"
       StaffMailer.incoming_mail_event(msg).deliver_now
       is_ok
@@ -15,16 +15,28 @@ class Griddler::AuthenticationController < Griddler::EmailsController
   end
 
   private
-
   def unauthorized
-    render nothing: true, status: :unauthorized
+    render nothing: true, status: :unauthorized and return
   end
 
   def unavailable
-    render nothing: true, status: :service_unavailable
+    render nothing: true, status: :service_unavailable and return
   end
 
   def is_ok
     render nothing: true, status: :ok
+  end
+
+  protected
+  def authenticate
+    verify_sparkpost_token || unauthorized
+  end
+
+  def verify_sparkpost_token
+    request.headers["X-MessageSystems-Webhook-Token"] == valid_token?
+  end
+
+  def valid_token?
+    GetSetting.site_setting('SPARKPOST_AUTH_TOKEN')
   end
 end
