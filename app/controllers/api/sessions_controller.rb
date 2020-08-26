df86a8270b8778346021ae442f16d13c0b926501
Brent Kearney <brent@netmojo.ca>
2020-08-26 17:00:20 -0600

    Allow organizers to have JSON API access

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -17,13 +17,13 @@ class Api::SessionsController < Devise::SessionsController
       render status: 401, json: { response: "Access denied." } and return
     end
 
-    sign_out_and_respond(resource) and return unless resource.staff?
+    sign_out_and_respond(resource) and return unless resource.allow_api_access?
 
     sign_in(resource_name, resource)
     respond_with resource, location: after_sign_in_path_for(resource) do |format|
       format.json { render status: 200,
                              json: { success: true, jwt: current_token,
-                                  response: "Authentication successful" } }
+                                  message: "Authentication successful" } }
     end
   end
 
@@ -36,14 +36,14 @@ class Api::SessionsController < Devise::SessionsController
   def check_request_format
     unless request.format == :json
       sign_out
-      render status: 406, json: { message: "JSON requests only." } and return
+      render status: 406, json: { success: false, message: "JSON requests only." } and return
     end
   end
 
   def sign_out_and_respond(resource)
     revoke_token(resource)
     sign_out(resource)
-    render status: 401, json: { success: false, response: "Staff access only." }
+    render status: 401, json: { success: false, message: "No API access allowed." }
   end
 
   def current_token
